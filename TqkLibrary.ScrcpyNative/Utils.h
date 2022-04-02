3d40725fe267d47a7d911cc522fcc33c6393b991
tqk2811 <tqk2811@gmail.com>
2022-04-03 00:09:36 +0700

    utils add avcheck

--- TqkLibrary.ScrcpyNative/Utils.h
+++ TqkLibrary.ScrcpyNative/Utils.h
@@ -5,8 +5,23 @@ static inline void arlet(const char* s) {
 	::MessageBoxA(NULL, (LPCSTR)s, (LPCSTR)"ShaderErr", MB_OK);
 }
 
+//#define avcheck(x) if(x < 0){\
+//	char buffer[1024];\
+//	av_strerror(err, buffer, 1024);\
+//	::MessageBoxA(NULL, (LPCSTR)buffer, (LPCSTR)"Libav", MB_OK);\
+//	return false;\
+//}
 
 
+static inline bool avcheck(int err) {
+	if (err < 0) {
+		char buffer[1024];
+		av_strerror(err, buffer, 1024);
+		::MessageBoxA(NULL, (LPCSTR)buffer, (LPCSTR)"Libav", MB_OK);
+		return false;
+	}
+	return true;
+}
 
 
 
