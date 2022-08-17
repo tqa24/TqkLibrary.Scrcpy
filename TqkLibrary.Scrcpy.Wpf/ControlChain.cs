9851f010b3807ca5e0023838aabbb9a8131b447b
tqk2811 <tqk2811@gmail.com>
2022-08-17 12:46:35 +0700

    fix ControlChain All extension

--- TqkLibrary.Scrcpy.Wpf/ControlChain.cs
+++ TqkLibrary.Scrcpy.Wpf/ControlChain.cs
@@ -25,23 +25,7 @@ namespace TqkLibrary.Scrcpy.Wpf
 
         protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
         {
-            if ((e.Action == NotifyCollectionChangedAction.Remove ||
-                e.Action == NotifyCollectionChangedAction.Reset ||
-                e.Action == NotifyCollectionChangedAction.Replace) &&
-
-                e?.OldItems != null)
-            {
-                foreach (var item in e.OldItems.Cast<IControl>())
-                {
-                    item.OnClipboardReceived -= Item_OnClipboardReceived;
-                    item.OnSetClipboardAcknowledgement -= Item_OnSetClipboardAcknowledgement;
-                }
-            }
-
-            if ((e.Action == NotifyCollectionChangedAction.Add ||
-                e.Action == NotifyCollectionChangedAction.Replace) &&
-
-                e?.NewItems != null)
+            if (e?.NewItems != null)
             {
                 foreach (var item in e.NewItems.Cast<IControl>())
                 {
@@ -49,7 +33,14 @@ namespace TqkLibrary.Scrcpy.Wpf
                     item.OnSetClipboardAcknowledgement += Item_OnSetClipboardAcknowledgement;
                 }
             }
-
+            if (e?.OldItems != null)
+            {
+                foreach (var item in e.OldItems.Cast<IControl>())
+                {
+                    item.OnClipboardReceived -= Item_OnClipboardReceived;
+                    item.OnSetClipboardAcknowledgement -= Item_OnSetClipboardAcknowledgement;
+                }
+            }
             base.OnCollectionChanged(e);
         }
 
@@ -67,58 +58,58 @@ namespace TqkLibrary.Scrcpy.Wpf
         public event OnDataReceived<long> OnSetClipboardAcknowledgement;
 
         public bool BackOrScreenOn(AndroidKeyEventAction KeyEventAction)
-            => this.All(x => x.BackOrScreenOn(KeyEventAction));
+            => this.FixAll(x => x.BackOrScreenOn(KeyEventAction));
         public bool CollapsePanel()
-            => this.All(x => x.CollapsePanel());
+            => this.FixAll(x => x.CollapsePanel());
 
         public bool ExpandNotificationPanel()
-            => this.All(x => x.ExpandNotificationPanel());
+            => this.FixAll(x => x.ExpandNotificationPanel());
 
         public bool ExpandSettingsPanel()
-             => this.All(x => x.ExpandSettingsPanel());
+             => this.FixAll(x => x.ExpandSettingsPanel());
 
         public bool GetClipboard(CopyKey copyKey = CopyKey.None)
-            => this.First().GetClipboard(copyKey);//this.All(x => x.GetClipboard(copyKey));
+            => this.First().GetClipboard(copyKey);
 
         public bool InjectKeycode(AndroidKeyEventAction action, AndroidKeyCode keycode, uint repeat = 1, AndroidKeyEventMeta metaState = AndroidKeyEventMeta.META_NONE)
-            => this.All(x => x.InjectKeycode(action, keycode, repeat, metaState));
+            => this.FixAll(x => x.InjectKeycode(action, keycode, repeat, metaState));
 
         public bool InjectScrollEvent(Rectangle position, int vScroll, int hScroll = 0, AndroidMotionEventButton button = AndroidMotionEventButton.BUTTON_PRIMARY)
-            => this.All(x => x.InjectScrollEvent(new Rectangle(position.Location, x.Scrcpy.ScreenSize), vScroll, hScroll, button));
+            => this.FixAll(x => x.InjectScrollEvent(new Rectangle(position.Location, x.Scrcpy.ScreenSize), vScroll, hScroll, button));
 
         public bool InjectText(string text)
-            => this.All(x => x.InjectText(text));
+            => this.FixAll(x => x.InjectText(text));
 
         public bool InjectTouchEvent(AndroidMotionEventAction action, long pointerId, Rectangle position, float pressure = 1, AndroidMotionEventButton buttons = AndroidMotionEventButton.BUTTON_PRIMARY)
-            => this.All(x => x.InjectTouchEvent(action, pointerId, new Rectangle(position.Location, x.Scrcpy.ScreenSize), pressure, buttons));
+            => this.FixAll(x => x.InjectTouchEvent(action, pointerId, new Rectangle(position.Location, x.Scrcpy.ScreenSize), pressure, buttons));
 
         public bool RotateDevice()
-            => this.All(x => x.RotateDevice());
+            => this.FixAll(x => x.RotateDevice());
 
         public bool SetClipboard(string text, bool paste)
-            => this.All(x => x.SetClipboard(text, paste));
+            => this.FixAll(x => x.SetClipboard(text, paste));
 
         public bool SetClipboard(string text, bool paste, long sequence)
-            => this.All(x => x.SetClipboard(text, paste, sequence));
+            => this.FixAll(x => x.SetClipboard(text, paste, sequence));
 
         public bool SetScreenPowerMode(ScrcpyScreenPowerMode powerMode)
-            => this.All(x => x.SetScreenPowerMode(powerMode));
+            => this.FixAll(x => x.SetScreenPowerMode(powerMode));
 
         #endregion
     }
 
 
-    //internal static class ControlChainExtension
-    //{
-    //    internal static bool All<TIn>(this IEnumerable<TIn> list, Func<TIn, bool> func)
-    //    {
-    //        var result = true;
-    //        foreach (var item in list)
-    //        {
-    //            if (result) result = func.Invoke(item);
-    //            else func.Invoke(item);
-    //        }
-    //        return result;
-    //    }
-    //}
+    internal static class ControlChainExtension
+    {
+        internal static bool FixAll<TIn>(this IEnumerable<TIn> list, Func<TIn, bool> func)
+        {
+            var result = true;
+            foreach (var item in list)
+            {
+                if (result) result = func.Invoke(item);
+                else func.Invoke(item);
+            }
+            return result;
+        }
+    }
 }
