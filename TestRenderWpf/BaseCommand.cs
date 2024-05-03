ccb256601d9c74ed95c3b8fcb757aa0c77d361d2
tqk2811 <tqk2811@gmail.com>
2024-05-03 16:04:10 +0700

    test wpf add button

---
+++ TestRenderWpf/BaseCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TestRenderWpf
+{
+    public class BaseCommand : ICommand
+    {
+        readonly Action _action;
+        public BaseCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _action();
+        }
+    }
+}
--- TestRenderWpf/MainWindowVM.cs
+++ TestRenderWpf/MainWindowVM.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 using TqkLibrary.WpfUi;
 using TqkLibrary.Scrcpy;
 using TqkLibrary.Scrcpy.Interfaces;
+using System.Windows;
+using TqkLibrary.Scrcpy.Enums;
 
 namespace TestRenderWpf
 {
     public class MainWindowVM : BaseViewModel
     {
+        public MainWindowVM()
+        {
+            InjectTextCommand = new BaseCommand(_InjectTextCommand);
+
+            ScreenPowerModes = Enum.GetValues<ScrcpyScreenPowerMode>();
+            SetScreenPowerModeCommand = new BaseCommand(_SetScreenPowerModeCommand);
+
+            AndroidKeyEventActions = Enum.GetValues<AndroidKeyEventAction>();
+            BackOrScreenOnCommand = new BaseCommand(_BackOrScreenOnCommand);
+
+            ExpandNotificationPanelCommand = new BaseCommand(_ExpandNotificationPanelCommand);
+            ExpandSettingsPanelCommand = new BaseCommand(_ExpandSettingsPanelCommand);
+            CollapsePanelCommand = new BaseCommand(_CollapsePanelCommand);
+            RotateDeviceCommand = new BaseCommand(_RotateDeviceCommand);
+            OpenHardKeyboardSettingCommand = new BaseCommand(_OpenHardKeyboardSettingCommand);
+        }
 
         IControl? _control;
         public IControl? Control
@@ -31,6 +49,79 @@ namespace TestRenderWpf
             get { return _ScrcpyUiView; }
             set { _ScrcpyUiView = value; NotifyPropertyChange(); }
         }
-    }
 
+
+
+        public BaseCommand InjectTextCommand { get; }
+        void _InjectTextCommand()
+        {
+            string text = Clipboard.GetText();
+            if (string.IsNullOrWhiteSpace(text)) text = "InjectText";
+            Control?.InjectText(text);
+        }
+
+
+        public IEnumerable<ScrcpyScreenPowerMode> ScreenPowerModes { get; }
+        ScrcpyScreenPowerMode _ScreenPowerModeSelected = ScrcpyScreenPowerMode.POWER_MODE_NORMAL;
+        public ScrcpyScreenPowerMode ScreenPowerModeSelected
+        {
+            get { return _ScreenPowerModeSelected; }
+            set { _ScreenPowerModeSelected = value; NotifyPropertyChange(); }
+        }
+        public BaseCommand SetScreenPowerModeCommand { get; }
+        void _SetScreenPowerModeCommand()
+        {
+            Control?.SetScreenPowerMode(ScreenPowerModeSelected);
+        }
+
+
+        public IEnumerable<AndroidKeyEventAction> AndroidKeyEventActions { get; }
+
+        AndroidKeyEventAction _AndroidKeyEventActionSelected = AndroidKeyEventAction.ACTION_DOWN;
+        public AndroidKeyEventAction AndroidKeyEventActionSelected
+        {
+            get { return _AndroidKeyEventActionSelected; }
+            set { _AndroidKeyEventActionSelected = value; NotifyPropertyChange(); }
+        }
+        public BaseCommand BackOrScreenOnCommand { get; }
+        void _BackOrScreenOnCommand()
+        {
+            Control?.BackOrScreenOn(AndroidKeyEventActionSelected);
+        }
+
+
+        public BaseCommand ExpandNotificationPanelCommand { get; }
+        void _ExpandNotificationPanelCommand()
+        {
+            Control?.ExpandNotificationPanel();
+        }
+
+
+        public BaseCommand ExpandSettingsPanelCommand { get; }
+        void _ExpandSettingsPanelCommand()
+        {
+            Control?.ExpandSettingsPanel();
+        }
+
+
+        public BaseCommand CollapsePanelCommand { get; }
+        void _CollapsePanelCommand()
+        {
+            Control?.CollapsePanel();
+        }
+
+
+        public BaseCommand RotateDeviceCommand { get; }
+        void _RotateDeviceCommand()
+        {
+            Control?.RotateDevice();
+        }
+
+
+        public BaseCommand OpenHardKeyboardSettingCommand { get; }
+        void _OpenHardKeyboardSettingCommand()
+        {
+            Control?.OpenHardKeyboardSetting();
+        }
+    }
 }
